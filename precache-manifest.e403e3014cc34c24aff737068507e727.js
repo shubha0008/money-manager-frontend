f4a2ad6462968afd66290c73fdfd0f441312c7ac
shubha0008 <shubharashmi101@gmail.com>
2020-10-07 13:49:28 +0530

    Updates

---
+++ precache-manifest.e403e3014cc34c24aff737068507e727.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f59bd14167348958b52bd3762812b6",
+    "url": "/money-manager-frontend/index.html"
+  },
+  {
+    "revision": "78d19b6e41157b43c8e7",
+    "url": "/money-manager-frontend/static/css/main.66b05358.chunk.css"
+  },
+  {
+    "revision": "2529f51417e85ee6b6d8",
+    "url": "/money-manager-frontend/static/js/2.13eb807c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/money-manager-frontend/static/js/2.13eb807c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78d19b6e41157b43c8e7",
+    "url": "/money-manager-frontend/static/js/main.ab1a2f00.chunk.js"
+  },
+  {
+    "revision": "3b9cde7254ccb7700c71",
+    "url": "/money-manager-frontend/static/js/runtime-main.a43ae6f7.js"
+  }
+]);
