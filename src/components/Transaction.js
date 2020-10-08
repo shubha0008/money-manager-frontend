8271ed729f10ddf7d8b4f716ff99a6baad869504
shubha0008 <shubharashmi101@gmail.com>
2020-10-08 13:09:59 +0530

    later commit

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,26 +1,45 @@
-import React, {useContext} from 'react';
+import React, {useContext , useEffect , useState} from 'react';
 import { GlobalContext } from '../context/GlobalState';
 
 export const Transaction = ({ transaction }) => {
   const { deleteTransaction } = useContext(GlobalContext);
+  const [data,setData] = useState([])
 
   const sign = transaction.amount < 0 ? '-' : '+';
 
 //   useEffect(()=>{
         
 //     fetch("https://app-demo-new.herokuapp.com/products", {
-//         method : "GET",
+//         method : "DELETE",
 
 //     }).then(res => res.json())
 //     .then((data) => {
-//     console.log({data})
+//       setData(data)
 //   }).catch((err) => {
 //     console.log("error");
-//   })
+//   }) 
 // })
+const deleteTransaction = async(e) => {
+  e.preventDefault();
+
+  try{
+    fetch("https://app-demo-new.herokuapp.com/products", {
+        method : "DELETE",
+
+    }).then(res => res.json())
+    .then((data) => {
+      setData(data)
+  }).catch((err) => {
+    console.log("error");
+  }) 
+  }catch (error){
+    console.log(error);
+}
+}
 
   return (
     <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+      
       {transaction.text} <span>{sign}₹{Math.abs(transaction.amount)}</span><button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
     </li>
   )
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,18 +1,19 @@
-import React, { useContext , useEffect } from 'react';
+import React, { useContext , useEffect, useState } from 'react';
 import { Transaction } from './Transaction';
 
 import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
+  const [data,setData] = useState([])
   useEffect(()=>{
         
     fetch("https://app-demo-new.herokuapp.com/products", {
         method : "GET",
 
-    }).then(res => res.json())
+    }).then(response =>   response.json())
     .then((data) => {
-    console.log({data})
+    setData(data)
   }).catch((err) => {
     console.log("error");
   })
@@ -21,7 +22,7 @@ export const TransactionList = () => {
     <div>
       <h3>History</h3>
       <ul className="list">
-        {transactions.map(transaction => 
+        {data.map(transaction => 
         (<Transaction key={transaction.id} transaction={transaction} />))
         }
         
